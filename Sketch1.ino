168803e300eea55dde99ae44d719fd8fd097ccad
nazman <nazmang@gmail.com>
2017-11-24 22:32:52 +0200

    Before new branch

--- Sketch1.ino
+++ Sketch1.ino
@@ -7,11 +7,15 @@
 #define MY_NODE_ID 51
 
 // Enable debug prints to serial monitor
-//#define MY_DEBUG 
+#define MY_DEBUG 
+
+//#define MY_DEBUG_VERBOSE_RF24
 
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
  //#define MY_RADIO_RFM69
+
+#define MY_RF24_PA_LEVEL RF24_PA_MAX
 
 // For Mega2560
 #define MY_RF24_CE_PIN 49
@@ -56,23 +60,37 @@ public:
 		_state = 0; 
 		_msg = new MyMessage(_address, V_STATUS);
 	};
-
 	void begin() {
 		pinMode(_pin, OUTPUT);
 		digitalWrite(_pin, RELAY_OFF);
 		_state = loadState(_address);
-		digitalWrite(_pin, _state ? RELAY_ON : RELAY_OFF);		
+		Serial.print("Relay: ");
+		Serial.print(_address);
+		Serial.print(" .Loading previous state: ");
+		Serial.println(_state);
+#ifdef INVERSE_LOGIC
+		//digitalWrite(_pin, _state ? RELAY_OFF : RELAY_ON);
+		_state ? off() : on();
+#else
+		_state ? on() : off();
+		//digitalWrite(_pin, _state ? RELAY_ON : RELAY_OFF);
+#endif
+				
 	};
+
 	bool proceed(long start_time, long duration) {
 		
 		if (cur_time == 0) { return 0; }
 		if ((cur_time >= start_time) && cur_time < (start_time + duration) && _state == RELAY_OFF) {
-			this->on();
-			
+			Serial.print("Powering on by timer relay: ");
+			Serial.print(_address);			
+			this->on();			
 		}
 		else if (_state == RELAY_ON) { this->off(); }
+		
 		return _state;
 	};
+
 	void on() {
 		digitalWrite(_pin, RELAY_ON);
 		_state = RELAY_ON;
@@ -80,9 +98,9 @@ public:
 		Serial.print("Relay: ");
 		Serial.print(_address);
 		Serial.println(" is ON");
-
-		send(_msg->set(_state ? RELAY_ON : RELAY_OFF), true);
+		sendState();
 	};
+
 	void off() {
 		digitalWrite(_pin, RELAY_OFF);
 		_state = RELAY_OFF;
@@ -90,9 +108,19 @@ public:
 		Serial.print("Relay: ");
 		Serial.print(_address);
 		Serial.println(" is OFF");
-		send(_msg->set(_state ? RELAY_ON : RELAY_OFF), true);
+		sendState();
 	};
+
 	bool getState() { return _state; };
+	void sendState(bool ack = true) { 
+		wait(20);
+		if (send(_msg->set(_state ? RELAY_ON : RELAY_OFF), ack) == false)
+		{
+			wait(50);
+			send(_msg->set(_state ? RELAY_ON : RELAY_OFF), ack);
+		}
+
+	}
 	bool getAddr() { return _address; };
 
 private:
@@ -103,64 +131,17 @@ private:
 };
 
 
-//long cur_time;
-//long rise_start, set_start;
-
 long filter_time, co2_time, oxygen_time; //variables for starting time
 long filter_duration, co2_duration, oxygen_duration; //duration variables
 
 bool timeReceived = false;
 unsigned long lastRequest = 0;
 
-//bool state1, state2, state3, state4;
-//bool cur_state;
-
-/*MyMessage msg(CHILD_ID_RELAY1, V_STATUS);
-MyMessage msg2(CHILD_ID_RELAY2, V_STATUS);
-MyMessage msg3(CHILD_ID_RELAY3, V_STATUS);
-MyMessage msg4(CHILD_ID_RELAY4, V_STATUS);*/
-
 Relay r1(RELAY1_PIN, CHILD_ID_RELAY1);
 Relay r2(RELAY2_PIN, CHILD_ID_RELAY2);
 Relay r3(RELAY3_PIN, CHILD_ID_RELAY3);
 Relay r4(RELAY4_PIN, CHILD_ID_RELAY4);
 
-/*bool control_relay(uint8_t pin, uint8_t id, long start_time, long duration, bool state){
-	// checking for state
-	if (loadState(id) == state) return;
-	// checking for time when ON/OFF relay
-	if ((cur_time >= start_time) && cur_time < (start_time + duration)){
-		digitalWrite(pin, RELAY_ON); 
-		saveState(id, RELAY_ON);
-		return true;
-	}
-	else {
-		digitalWrite(pin, RELAY_OFF); 
-		saveState(id, RELAY_OFF);
-		return false;
-	}
-
-}*/
-
-void presentation() {
-	// Send the sketch version information to the gateway and Controller
-	sendSketchInfo("Aquas", "1.0");
-
-	// Register all sensors to gw (they will be created as child devices)
-	present(CHILD_ID_RELAY1, S_BINARY);
-	present(CHILD_ID_RELAY2, S_BINARY);
-	present(CHILD_ID_RELAY3, S_BINARY);
-	present(CHILD_ID_RELAY4, S_BINARY);
-}
-
-// This is called when a new time value was received
-void receiveTime(unsigned long controllerTime) {
-	// Ok, set incoming time 
-	Serial.print("Time value received: ");
-	Serial.println(controllerTime);
-	RTC.set(controllerTime); // this sets the RTC to the time from controller - which we do want periodically
-	timeReceived = true;
-}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -170,14 +151,14 @@ void setup() {
 
 	// the function to get the time from the RTC
 	setSyncProvider(RTC.get);
-	
+
 	// Request latest time from controller at startup
 	requestTime();
 
 	// sunset params
-	/*rise_start = 7 * SECS_PER_HOUR + 15 * SECS_PER_MIN; 
+	/*rise_start = 7 * SECS_PER_HOUR + 15 * SECS_PER_MIN;
 	set_start = 16 * SECS_PER_HOUR + 45 * SECS_PER_MIN; */
-		
+
 	// small aquas
 	/*filter_time = 12 * SECS_PER_HOUR + 30 * SECS_PER_MIN; // 10:30 AM
 	co2_time = 12 * SECS_PER_HOUR + 20 * SECS_PER_MIN;;
@@ -186,7 +167,7 @@ void setup() {
 	filter_duration = 8 * SECS_PER_HOUR + 0 * SECS_PER_MIN; // 8 hours
 	co2_duration = 9 * SECS_PER_HOUR;
 	oxygen_duration = 2 * SECS_PER_HOUR; //2 hours*/
-	
+
 	// big aquas
 	filter_time = 7 * SECS_PER_HOUR + 0 * SECS_PER_MIN;
 	co2_time = 7 * SECS_PER_HOUR + 0 * SECS_PER_MIN;
@@ -207,7 +188,7 @@ void setup() {
 	pinMode(RELAY3_PIN, OUTPUT);
 	pinMode(RELAY1_PIN, OUTPUT);
 	pinMode(RELAY2_PIN, OUTPUT);
-	
+
 
 	// Make sure relays are off when starting up
 	digitalWrite(RELAY1_PIN, RELAY_OFF);
@@ -229,8 +210,39 @@ void setup() {
 	//digitalWrite(RELAY3_PIN, state3 ? RELAY_ON : RELAY_OFF);
 	//state4 = loadState(CHILD_ID_RELAY4);
 	//digitalWrite(RELAY4_PIN, state4 ? RELAY_ON : RELAY_OFF);
-	
+
 }
+
+void presentation() {
+	// Send the sketch version information to the gateway and Controller
+	sendSketchInfo("Aquas", "1.0",true);
+
+	// Register all sensors to gw (they will be created as child devices)
+	present(CHILD_ID_RELAY1, S_BINARY);
+	present(CHILD_ID_RELAY2, S_BINARY);
+	present(CHILD_ID_RELAY3, S_BINARY);
+	present(CHILD_ID_RELAY4, S_BINARY);
+
+	/*r1.sendState(false);
+	wait(25);
+	r2.sendState(false);
+	wait(25);
+	r3.sendState(false);
+	wait(25);
+	r4.sendState(false);
+	wait(25);*/
+}
+
+// This is called when a new time value was received
+void receiveTime(unsigned long controllerTime) {
+	// Ok, set incoming time 
+	Serial.print("Time value received: ");
+	Serial.println(controllerTime);
+	RTC.set(controllerTime); // this sets the RTC to the time from controller - which we do want periodically
+	timeReceived = true;
+}
+
+
 
 // the loop function runs over and over again until power down or reset
 void loop() {
@@ -247,26 +259,22 @@ void loop() {
 	}
 
 	cur_time = elapsedSecsToday(RTC.get());
-	r1.proceed(filter_time, filter_duration);
+	/*r1.proceed(filter_time, filter_duration);
 	r2.proceed(co2_time, co2_duration);
 	r3.proceed(oxygen_time, oxygen_duration);
-	r4.proceed(oxygen_time, oxygen_duration);
-	/*send(msg.set(state1 ? true : false), true);
-	send(msg2.set(state2 ? true : false), true);
-	send(msg3.set(state3 ? true : false), true);
-	send(msg4.set(state4 ? true : false), true);*/	
+	r4.proceed(oxygen_time, oxygen_duration);*/
 	
-	smartSleep(1000);
-	sendHeartbeat();
+	wait(1000);
+	//sendHeartbeat();
 }
 
 void receive(const MyMessage &message) {
 	bool prevState, newState;
 	// We only expect one type of message from controller. But we better check anyway.
-	/*if (message.isAck()) {
+	if (message.isAck()) {
 	Serial.println("This is an ack from gateway");
 	return;
-	}*/
+	}
 	
 	if (message.type == V_STATUS) {
 		// Write some debug info
@@ -278,7 +286,7 @@ void receive(const MyMessage &message) {
 #else
 		Serial.println(message.getBool());
 #endif
-	/*	switch (message.sensor) {
+		switch (message.sensor) {
 
 		case CHILD_ID_RELAY1: {
 			prevState = r1.getState();
@@ -287,9 +295,9 @@ void receive(const MyMessage &message) {
 #endif	
 			newState = message.getBool();
 			// Change relay state
-			if (newState != prevState) {
+			//if (newState != prevState) {
 				newState ? r1.on() : r1.off();
-			}
+			//}
 			break;
 		}
 		case CHILD_ID_RELAY2: {
@@ -299,9 +307,9 @@ void receive(const MyMessage &message) {
 #endif
 			newState = message.getBool();
 			// Change relay state
-			if (newState != prevState) {
+			//if (newState != prevState) {
 				newState ? r2.on() : r2.off();
-			}
+			//}
 			break;
 		}
 		case CHILD_ID_RELAY3: {
@@ -311,9 +319,9 @@ void receive(const MyMessage &message) {
 #endif
 			newState = message.getBool();
 			// Change relay state
-			if (newState != prevState) {
+			//if (newState != prevState) {
 				newState ? r3.on() : r3.off();
-			}
+			//}
 			break;
 		}
 		case CHILD_ID_RELAY4: {
@@ -323,12 +331,12 @@ void receive(const MyMessage &message) {
 #endif
 			newState = message.getBool();
 			// Change relay state
-			if (newState != prevState) {
+			//if (newState != prevState) {
 				newState ? r4.on() : r4.off();
-			}
+			//}
 			break;
 		}
 
-		}		*/
+		}		
 	}
 }
